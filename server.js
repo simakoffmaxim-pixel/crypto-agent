0a95e52c0814b9980ac7d72934d428335f5619ac
simakoffmaxim-pixel <simakoffmaxim@gmail.com>
2026-05-14 14:46:16 +0300

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.json());
+app.use(express.static(__dirname));
 
 const TWITTER_BEARER = process.env.TWITTER_BEARER;
 
